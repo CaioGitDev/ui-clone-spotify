d1634a339c667c663a33fc54736d02346dcbac62
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2023-04-29 21:35:47 +0100

    feat: separate components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { AlbumCardWide } from '@/Components/ui/AlbumCardWide';
+import { Footer } from '@/Components/ui/Footer';
+import { Sidebar } from '@/Components/ui/Sidebar';
 import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume, Maximize2 } from 'lucide-react';
 import Image from 'next/image';
 
@@ -7,45 +9,7 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-72 bg-zinc-950 p-6">
-
-          <div className='flex items-center gap-2'>
-            <div className='w-3 h-3 bg-red-500 rounded-full'></div>
-            <div className='w-3 h-3 bg-yellow-500 rounded-full'></div>
-            <div className='w-3 h-3 bg-green-500 rounded-full'></div>
-
-          </div>
-
-          <nav className='space-y-5 mt-10'>
-            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200 '>
-              <HomeIcon />
-              Home
-            </a>
-            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200 '>
-              <Search />
-              Search
-            </a>
-            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200 '>
-              <Library />
-              Your Library
-            </a>
-          </nav>
-
-          <nav className='mt-6 pt-10 border-t border-zinc-800 flex flex-col gap-3'>
-            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist Name</a>
-            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist Name</a>
-            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist Name</a>
-            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist Name</a>
-            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist Name</a>
-            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist Name</a>
-            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist Name</a>
-            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist Name</a>
-            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist Name</a>
-            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist Name</a>
-            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist Name</a>
-            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Playlist Name</a>
-          </nav>
-        </aside>
+        <Sidebar />
 
         <main className="flex-1 p-6">
           <div className='flex items-center gap-4'>
@@ -78,46 +42,9 @@ export default function Home() {
 
           </div>
         </main>
+
       </div>
-      <footer className="bg-zinc-900 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
-        <div className='flex items-center gap-3'>
-          <Image src="https://i.scdn.co/image/ab67706f00000002ca5a7517156021292e5663a6" alt="Album" width={56} height={56} />
-          <div className='flex flex-col '>
-            <strong className='font-normal'>Andromeda</strong>
-            <span className='text-xs text-zinc-400'>Esquece</span>
-          </div>
-        </div>
-        <div className='flex flex-col items-center gap-2'>
-          <div className='flex items-center gap-6'>
-            <Shuffle size={20} className='text-zinc-200' />
-            <SkipBack size={20} className='text-zinc-200' />
-            <button className='w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black'>
-              <Play />
-            </button>
-            <SkipForward size={20} className='text-zinc-200' />
-            <Repeat size={20} className='text-zinc-200' />
-          </div>
-          <div className='flex items-center gap-2'>
-            <span className='text-xs text-zinc-400'>0:55</span>
-            <div className='h-1 rounded-full w-96 bg-zinc-600'>
-              <div className='h-1 rounded-full w-40 bg-zinc-200'></div>
-            </div>
-            <span className='text-xs text-zinc-400'>3:06</span>
-          </div>
-        </div>
-        <div className='flex items-center gap-4'>
-          <Mic2 size={20} />
-          <LayoutList size={20} />
-          <Laptop2 size={20} />
-          <div className='flex items-center gap-2'>
-            <Volume size={20} />
-            <div className='h-1 rounded-full w-24 bg-zinc-600'>
-              <div className='h-1 rounded-full w-10 bg-zinc-200'></div>
-            </div>
-          </div>
-          <Maximize2 size={20} />
-        </div>
-      </footer>
+      <Footer />
     </div>
   )
 }
